5563bf85f8be4781716c7473514ded255dcb7d4c
24SergioA <sergio.algarin@g.austincc.edu>
2021-03-08 17:58:41 -0600

    HW - 4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -15,28 +15,7 @@ int factorial(int num)//num is 0
         
         i--;
     }
-    
-    std::cout<<"The Factorial of " <<  num << " is : " << fact << std::endl;
-   
-
-    /*
-    // num is the user's input
-    // digit is the varible that carries ' num '
-    int digit = num;
-
-    // sum will do the math and save the user's under a newer variable
-    int sum = digit;
-
-    // To make the variable empty
-    int total = 0;
-    
-    for (int i = sum - 1; i > 0; i--)
-    {
-        total = sum * i;
-    }
-    std::cout<<"factorial of " <<  digit << " is : " << total << std::endl;
-    */
-    
-    return 0;
+        
+    return fact;
     
 }
